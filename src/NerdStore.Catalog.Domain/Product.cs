70962db117198e0974e8ab80b3b5e6664bf971a1
Fernando Motta <fvvmotta@gmail.com>
2023-02-13 09:25:58 -0300

    Created Entity as base class to be called by other entities. Created Product and Category Class with validation via assertion concern.

---
+++ src/NerdStore.Catalog.Domain/Product.cs
@@ -0,0 +1,72 @@
+﻿
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Product : Entity, IAggregateRoot 
+    {
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime RegisterDate { get; private set; }
+        public string Image { get; private set; }
+        public int InventoryQuantity { get; private set; }
+        public Category Category { get; private set; }
+
+        public Product(string name, string description, bool active, decimal value, Guid categoryId, DateTime registerDate, string image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Value = value;
+            RegisterDate = registerDate;
+            Image = image;
+
+            Validate();
+        }
+
+        public void Activate() => Active = true;
+        public void Deactivate() => Active = false;
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            AssertionConcern.AssertArgumentIfEmpty(description, "Description Field of the product can not be empty");
+            Description = description;
+        }
+
+        public void DebitInventory(int quantity)
+        {
+            if (quantity < 0) quantity *= -1;
+            if (!HasInventory(quantity)) throw new DomainException("Insuficient stock");
+            InventoryQuantity -= quantity;
+        }
+
+        public void ReplenishInventory(int quantity)
+        {
+            InventoryQuantity += quantity;
+        }
+
+        public bool HasInventory(int quantity)
+        {
+            return InventoryQuantity >= quantity;
+        }
+
+        public void Validate()
+        {
+            AssertionConcern.AssertArgumentIfEmpty(Name, "Name of the product can not be empty");
+            AssertionConcern.AssertArgumentIfEmpty(Description, "Description of the product can not be empty");
+            AssertionConcern.AssertArgumentIfEquals(CategoryId, Guid.Empty, "Category Id of the product can not be empty");
+            AssertionConcern.AssertArgumentIfLessThanOrEqualsMinimun(Value, 1, "Value of the product can not be less than or equals to 0");
+            AssertionConcern.AssertArgumentIfEmpty(Image, "Image field of the product can not be empty");
+        }
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/AssertionConcern.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public class AssertionConcern
+    {
+        public static void AssertArgumentIfEquals(object object1, object object2, string message)
+        {
+            if (object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfNotEquals(object object1, object object2, string message)
+        {
+            if (!object1.Equals(object2))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfNotEquals(string pattern, string stringValue, string message)
+        {
+            Regex regex = new Regex(pattern);
+
+            if (!regex.IsMatch(stringValue))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentLength(string stringValue, int maximum, string message)
+        {
+            int length = stringValue.Trim().Length;
+            if (length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentLength(string stringValue, int minimum, int maximum, string message)
+        {
+            int length = stringValue.Trim().Length;
+            if (length < minimum || length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentExpression(string pattern, string value, string message)
+        {
+            var regex = new Regex(pattern);
+            if (!regex.IsMatch(value))
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfEmpty(string stringValue, string message)
+        {
+            if (stringValue == null || stringValue.Trim().Length == 0)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfNull(object object1, string message)
+        {
+            if (object1 == null)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentRange(double value, double minimum, double maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentRange(float value, float minimum, float maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentRange(int value, int minimum, int maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentRange(long value, long minimum, long maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentRange(decimal value, decimal minimum, decimal maximum, string message)
+        {
+            if (value < minimum || value > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfLessThanOrEqualsMinimun(double value, double minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfLessThanOrEqualsMinimun(float value, float minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfLessThanOrEqualsMinimun(int value, int minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfLessThanOrEqualsMinimun(long value, long minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfLessThanOrEqualsMinimun(decimal value, decimal minimum, string message)
+        {
+            if (value <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfFalse(bool boolValue, string message)
+        {
+            if (boolValue)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertArgumentIfTrue(bool boolValue, string message)
+        {
+            if (!boolValue)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertStateIfFalse(bool boolValue, string message)
+        {
+            if (boolValue)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void AssertStateIfTrue(bool boolValue, string message)
+        {
+            if (!boolValue)
+            {
+                throw new DomainException(message);
+            }
+        }
+    }
+}
